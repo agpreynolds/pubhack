93bb478c50114ca54777c7667956fd3b59c6c5f6
Alex Reynolds <cam00674@myport.ac.uk>
2014-02-18 11:44:37 +0000

    Adding config file

---
+++ php/configuration/twitter.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'oauth_access_token' => "843572402-Sajiyzb3vnUimZbGrs0AL7w1lv1YL5bWwMH49AKT",
+    'oauth_access_token_secret' => "To2XA4xKmvvWH5OZnBYc1pH1hs6WcHXElbdf8oaOhbf5b",
+    'consumer_key' => "vHKPW4ZjmfKrn4GlFQEvbw",
+    'consumer_secret' => "bXWZqdIpUuuLl5k6zh57TbYFRgcX7XexSaIGVQlWo"
+);
+
+?>
